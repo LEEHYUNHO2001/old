d6abdf537d945c661fc39dba71f48c0d3395aea9
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2021-04-20 15:25:17 +0900

    코덱 정보 조사

--- ffmpegTest/ffmpegTest.cpp
+++ ffmpegTest/ffmpegTest.cpp
@@ -16,14 +16,17 @@ extern "C" {
 
 AVFormatContext* fmtCtx;
 int vidx = -1, aidx = -1;
-AVStream* vStream, * aStream;
+AVStream * vStream, * aStream;
 AVCodecParameters* vPara, * aPara;
+AVCodec * vCodec, * aCodec;
+AVCodecContext * vCtx, * aCtx;
 
 int main(void) {
     int ret = avformat_open_input(&fmtCtx, "C:\\ffstudy\\sample.mp4", NULL, NULL);
     if (ret != 0) { return -1; }
     avformat_find_stream_info(fmtCtx, NULL);
 
+    //비디오, 오디오 스트림 첨자
     vidx = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
     aidx = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_AUDIO, -1, vidx, NULL, 0);
 
@@ -56,6 +59,30 @@ int main(void) {
     printf("코덱 = %d\n", aPara->codec_id);
     printf("채널 = %d\n", aPara->channels);
     printf("샘플 레이트 = %d\n", aPara->sample_rate);
+
+    // 비디오 코덱 오픈
+    vStream = fmtCtx->streams[vidx];
+    vPara = vStream->codecpar;
+    vCodec = avcodec_find_decoder(vPara->codec_id);
+    vCtx = avcodec_alloc_context3(vCodec);
+    avcodec_parameters_to_context(vCtx, vPara);
+    avcodec_open2(vCtx, vCodec, NULL);
+
+    // 오디오 코덱 오픈
+    aStream = fmtCtx->streams[aidx];
+    aPara = aStream->codecpar;
+    aCodec = avcodec_find_decoder(aPara->codec_id);
+    aCtx = avcodec_alloc_context3(aCodec);
+    avcodec_parameters_to_context(aCtx, aPara);
+    avcodec_open2(aCtx, aCodec, NULL);
+
+    printf("------코덱 정보 조사--------\n");
+    printf("비디오 코덱 : %d, %s(%s)\n", vCodec->id, vCodec->name, vCodec->long_name);
+    printf("능력치 : %x\n", vCodec->capabilities);
+    printf("오디오 코덱 : %d, %s(%s)\n", aCodec->id, aCodec->name, aCodec->long_name);
+    printf("능력치 : %x\n", aCodec->capabilities);
+    avcodec_free_context(&vCtx);
+    avcodec_free_context(&aCtx);
 
     avformat_close_input(&fmtCtx);
     return 0;

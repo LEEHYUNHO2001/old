961185cf3602e35561e7a45923f17740a23940e8
LEEHYUNHO2001 <dlgusgh2001@naver.com>
2021-04-28 15:12:52 +0900

    버퍼 구조 -> 헤더 버퍼 동적 할당

--- WaveformTest/WaveformTest.cpp
+++ WaveformTest/WaveformTest.cpp
@@ -7,22 +7,23 @@
 HWAVEOUT hWaveDev;
 HANDLE hFile;
 DWORD dwRead;
-char* samplebuf;
-DWORD bufsize;
 WAVEFORMATEX wf;
-WAVEHDR hdr = { NULL, };
+const int hdrnum = 3;
+const int bufsize = 10000;
+long availhdr = hdrnum;
+int nowhdr = 0;
 
-// 대기를 위한 변수
-bool bWait = true;
+
 
 void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) {
     // 재생이 끝나면 대기를 풀어준다.
     if (uMsg == WOM_DONE) {
-        bWait = false;
+        InterlockedIncrement(&availhdr);
     }
 }
 
-int main() {
+int PlayWave(LPCTSTR song){
+    /*
     //사운드 출력 장치 조사
     UINT wavenum;
     char devname[128];
@@ -35,9 +36,10 @@ int main() {
         printf("%d번 : %d 채널,지원 포맷=%x,기능=%x,이름=%s\n",
             i, cap.wChannels, cap.dwFormats, cap.dwSupport, devname);
     }
-
+    */
+    WAVEHDR* hdr;
     // 웨이브 파일을 연다.
-    hFile = CreateFile(TEXT("c:\\ffstudy\\techno.wav"), GENERIC_READ, FILE_SHARE_READ, NULL,
+    hFile = CreateFile(song, GENERIC_READ, FILE_SHARE_READ, NULL,
        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     //에러처리
     if (hFile == INVALID_HANDLE_VALUE) {
@@ -47,31 +49,43 @@ int main() {
     // 재생 장치를 연다.
     wf.cbSize = sizeof(WAVEFORMATEX);
     wf.wFormatTag = WAVE_FORMAT_PCM;
-    wf.nChannels = 2;
-    wf.wBitsPerSample = 16;
-    wf.nSamplesPerSec = 44100;
+    SetFilePointer(hFile, 22, NULL, SEEK_SET); ReadFile(hFile, &wf.nChannels, 2, &dwRead, NULL);
+    SetFilePointer(hFile, 24, NULL, SEEK_SET); ReadFile(hFile, &wf.nSamplesPerSec, 4, &dwRead, NULL);
+    SetFilePointer(hFile, 34, NULL, SEEK_SET); ReadFile(hFile, &wf.wBitsPerSample, 2, &dwRead, NULL);
     wf.nBlockAlign = wf.nChannels * wf.wBitsPerSample / 8;
     wf.nAvgBytesPerSec = wf.nSamplesPerSec * wf.nBlockAlign;
     waveOutOpen(&hWaveDev, WAVE_MAPPER, &wf, (DWORD)waveOutProc, 0, CALLBACK_FUNCTION);
     // 헤더는 건너 뛰고 버퍼에 샘플 데이터를 읽어들인다.
     SetFilePointer(hFile, 44, NULL, SEEK_SET);
-    bufsize = wf.nAvgBytesPerSec;
-    samplebuf = (char*)malloc(bufsize);
-    hdr.lpData = samplebuf;
-    do {
-        ReadFile(hFile, samplebuf, bufsize, &dwRead, NULL);
-        printf("Read %d\n", dwRead);
-        hdr.dwBufferLength = dwRead;
-        waveOutPrepareHeader(hWaveDev, &hdr, sizeof(WAVEHDR));
-        waveOutWrite(hWaveDev, &hdr, sizeof(WAVEHDR));
-        // 대기가 풀릴 때까지 기다린다.
-        while (bWait) Sleep(0);
-        bWait = true;
-    } while (dwRead == bufsize);
-    // 뒷정리한다.
-    waveOutUnprepareHeader(hWaveDev, &hdr, sizeof(WAVEHDR));
-    free(samplebuf);
-    waveOutClose(hWaveDev);
+    //헤더와 버퍼 한번에 할당
+    hdr = (WAVEHDR*)calloc(hdrnum, sizeof(WAVEHDR) + bufsize);
+    char* bufstart = (char*)hdr + sizeof(WAVEHDR) * hdrnum;
+    for (int i = 0; i < hdrnum; i++) {
+        hdr[i].lpData = bufstart + bufsize * i;
+    }
+    // 파일을 다 읽을 때까지 루프를 돈다.
+    for (;;) {
+        ReadFile(hFile, hdr[nowhdr].lpData, bufsize, &dwRead, NULL);
+        if (dwRead == 0) break;
+        hdr[nowhdr].dwBufferLength = dwRead;
+        // 출력 장치로 전송
+        waveOutPrepareHeader(hWaveDev, &hdr[nowhdr], sizeof(WAVEHDR));
+        waveOutWrite(hWaveDev, &hdr[nowhdr], sizeof(WAVEHDR));
+        // 가용 버퍼 하나 감소
+        InterlockedDecrement(&availhdr);
+        // 가용 버퍼가 없으면 풀릴 때까지 대기
+        while (availhdr == 0) Sleep(20);
+        // 다음 버퍼 위치로 이동
+        if (++nowhdr == hdrnum) nowhdr = 0;
+    }
+    for (int i = 0; i < hdrnum; i++) {
+        waveOutUnprepareHeader(hWaveDev, &hdr[i], sizeof(WAVEHDR));
+    }
+    while (waveOutClose(hWaveDev) == WAVERR_STILLPLAYING) { Sleep(10); }
     CloseHandle(hFile);
     return 0;
+}
+
+int main() {
+    PlayWave(TEXT("c:\\ffstudy\\techno.wav"));
 }
